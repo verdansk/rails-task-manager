11f8efbcdac1992351e01166a11c69ba09ba7452
Maarten Bron <maarten@roads2ranking.com>
2020-11-17 14:26:40 +0100

    finished exercise 2, day 2 rails

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,42 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
   def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def create
+    @tasks = Task.new(tasks_params)
+    @tasks.save
+    redirect_to task_path(@tasks)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(tasks_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+private
+  def tasks_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
